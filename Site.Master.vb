036b87ef77eabd415c5645692bc737a254c12ad9
zulfaw <125231460+zulfaw@users.noreply.github.com>
2023-03-01 15:41:10 +0800

    Add files via upload

--- Site.Master.vb
+++ Site.Master.vb
@@ -5,15 +5,13 @@ Public Class SiteMaster
     Inherits MasterPage
     Dim Sql As String
     Dim uKodModul As String
-    Dim nostaf As String
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 
         uKodModul = Session("KodModul")
-        nostaf = Session("LOGINID")
 
         If Page.IsPostBack = False Then
 
-            '    Dim nostaf As String = Request.QueryString("UsrLogin")
+            Dim nostaf As String = Request.QueryString("UsrLogin")
             Dim uSession As String = Request.QueryString("usession")
 
 
@@ -25,7 +23,7 @@ Public Class SiteMaster
             If uKodModul <> "" Then
 
 
-                Sql = "select Kod_Sub, Nama_Sub, isnull(icon_location,'-') icon_location, Nama_Icon from SMKB_Sub_Modul where status = 1 and Kod_Modul = '" & uKodModul & "'
+                Sql = "select Kod_Sub, Nama_Sub, isnull(icon_location,'-') icon_location  from SMKB_Sub_Modul where status = 1 and Kod_Modul = '" & uKodModul & "'
                 order by Urutan"
                 dbcomm = New SqlCommand(Sql, dbconnNew)
 
