57f2dfa6522c3f4bc39d7644d00994328455d5b4
zulfaw <125231460+zulfaw@users.noreply.github.com>
2023-03-01 14:52:45 +0800

    Add files via upload

---
+++ Senarai_Invois.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/NestedMasterPage2.master" CodeBehind="Senarai_Invois.aspx.vb" Inherits="SMKB_Web_Portal.Senarai_Invois" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="FormContents" runat="Server">
+
+    <link type="text/css" rel="stylesheet" href="../../../Scripts/jquery 1.13.3/css/jquery.dataTables.min.css" />
+    <link type="text/css" rel="stylesheet" href="../../../Scripts/cdn/css/dataTables.bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="../../../Scripts/bootstrap 3.3.7/bootstrap.min.js" />
+    <link type="text/css" rel="stylesheet" href="../../../Scripts/cdn/css/responsive.bootstrap.min.css" />
+    <script type="text/javascript" src="../../../Scripts/jquery 3.5.1/jquery-3.5.1.js"></script>
+    <script type="text/javascript" src="../../../Scripts/jquery 1.13.3/js/jquery.dataTables.min.js"></script>
+
+    <script type="text/javascript">
+        $(function () {
+            $("[id*=gvSenarai]").prepend($("<thead></thead>").append($(this).find("tr:first"))).DataTable({
+                "pageLength":2,
+                "paging": true,
+                "lengthChange": false,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": true
+            });
+        })
+    </script>
+        
+    <div class="box-body" style="overflow: scroll; height: 300px">
+        <asp:GridView ID="gvSenarai" CssClass="table table-bordered table-striped" Width="100%" runat="server"
+            CellPadding="5" CellSpacing="2" AutoGenerateColumns="false">
+            <columns>                                                                     
+                <asp:TemplateField HeaderText="No.">
+                    <ItemTemplate>
+                        <%#Container.DataItemIndex + 1 %>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:BoundField DataField="NoInv" HeaderText="No. Invois" />
+                <asp:BoundField DataField="NPembekal" HeaderText="Pembekal" />
+
+            </Columns>
+        </asp:GridView>
+    </div>
+    
+</asp:Content>
+
---
+++ Senarai_Invois.aspx.vb
@@ -0,0 +1,92 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Drawing
+
+Public Class Senarai_Invois
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Try
+            If Not IsPostBack Then
+                'fBindDdlJenis()
+                'fBindDdlTahun()
+
+                fLoadSenarai()
+                'divList.Visible = True
+                'divDt.Visible = False
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub fLoadSenarai()
+        Try
+            Dim intRec As Integer
+            Dim strSql As String
+
+            'fClearGvSenarai()
+
+
+            Dim dt As New DataTable
+            dt.Columns.Add("IdInv", GetType(String))
+            dt.Columns.Add("NoInv", GetType(String))
+            dt.Columns.Add("NPembekal", GetType(String))
+            dt.Columns.Add("NoPT", GetType(String))
+            dt.Columns.Add("NoDO", GetType(String))
+            dt.Columns.Add("BekalKpd", GetType(String))
+            dt.Columns.Add("Jumlah", GetType(String))
+            dt.Columns.Add("Padan", GetType(Boolean))
+
+
+            ' strSql = "select a.AP01_NoId, a.AP01_NoInv, (select b.ROC01_NamaSya  from ROC01_Syarikat b where b.ROC01_IDSya = a.ROC01_IdSya ) as NPembekal,  a.PO19_NoPt , AP01_NoDo, (select c.PO19_BekalKepada from PO19_Pt c where c.PO19_NoPt = a.PO19_NoPt) as BekalKpd, AP01_Jumlah, AP01_Padan " &
+            '       "from AP01_Invois a where a.AP01_Status = 1 "
+
+            strSql = "select  * 
+                        from SMKB_Invois_Hdr a
+                        inner join  SMKB_Syarikat_Hdr b on b.No_Syarikat = a.No_Syarikat
+                        inner join PO19_Pt c on c.PO19_NoPt = a.No_PTempatan
+                        where a.Status = 1"
+            Dim ds As New DataSet
+            Dim dbconn As New DBKewConn
+            ds = dbconn.fSelectCommand(strSql)
+
+            Dim strIdInv, strNoInv, strNPembekal, strNoPT, strNoDO, strBekalKpd, strJumlah As String
+            Dim decAmaun As Decimal
+            Dim blnPadan As Boolean
+
+            If Not ds Is Nothing Then
+                If ds.Tables(0).Rows.Count > 0 Then
+                    For i As Integer = 0 To ds.Tables(0).Rows.Count - 1
+                        strIdInv = ds.Tables(0).Rows(i)("ID_Rujukan")
+                        strNoInv = ds.Tables(0).Rows(i)("No_Invois")
+                        strNPembekal = ds.Tables(0).Rows(i)("No_Syarikat")
+                        strNoPT = ds.Tables(0).Rows(i)("PO19_NoPt")
+                        strNoDO = ds.Tables(0).Rows(i)("No_Do")
+                        strBekalKpd = ds.Tables(0).Rows(i)("No_Syarikat")
+                        decAmaun = CDec(ds.Tables(0).Rows(i)("Jumlah_Sebenar"))
+                        strJumlah = FormatNumber(decAmaun, 2)
+                        blnPadan = CBool(ds.Tables(0).Rows(i)("Padan"))
+
+                        dt.Rows.Add(strIdInv, strNoInv, strNPembekal, strNoPT, strNoDO, strBekalKpd, strJumlah, blnPadan)
+                    Next
+                    gvSenarai.DataSource = dt
+                    gvSenarai.DataBind()
+                    ViewState("dtSenarai") = dt
+                    intRec = ds.Tables(0).Rows.Count
+                End If
+            End If
+
+            'lblJumRekod.InnerText = intRec
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Overloads Overrides Sub VerifyRenderingInServerForm(ByVal control As Control)
+        ' Verifies that the control is rendered
+    End Sub
+
+End Class
